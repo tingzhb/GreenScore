574dad8b3e5cc8ba17f73b6537fe5bbabe71ac8c
Benjamin Ting <tingzhb@gmail.com>
2022-09-22 09:19:58 +0200

    Make Stars

--- GreenScore/Assets/Scripts/UI/ShowItemDetails.cs
+++ GreenScore/Assets/Scripts/UI/ShowItemDetails.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class ShowItemDetails : MonoBehaviour{
 	[SerializeField] private TextMeshProUGUI[] texts;
+	[SerializeField] private GameObject[] stars;
 	private void Awake(){
 		Broker.Subscribe<ShowProductDetailsMessage>(OnShowProductDetailsMessageReceived);
 	}
@@ -13,10 +14,16 @@ public class ShowItemDetails : MonoBehaviour{
 		Broker.Unsubscribe<ShowProductDetailsMessage>(OnShowProductDetailsMessageReceived);
 	}
 	private void OnShowProductDetailsMessageReceived(ShowProductDetailsMessage obj){
-		Debug.Log("ItemFound!");
+
+		var greenScore = obj.GreenScore;
+		
 		texts[0].text = obj.ItemName;
 		texts[1].text = obj.ItemType.ToString();
-		texts[2].text = obj.GreenScore.ToString(CultureInfo.InvariantCulture);
+		texts[2].text = greenScore.ToString(CultureInfo.InvariantCulture);
 		texts[3].text = obj.Price.ToString(CultureInfo.InvariantCulture);
+
+		for (var i = 0; i < Mathf.Ceil(greenScore); i++){
+			stars[i].SetActive(true);
+		}
 	}
 }
